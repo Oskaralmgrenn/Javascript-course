1e66214c1eced9af5e36867af3d193a8b5d4604d
Oskar Almgren <oskaralmgren.oa@gmail.com>
2023-07-31 22:46:09 +0200

    Challenge 1 - BMI calculation

--- assignments.js
+++ assignments.js
@@ -1,72 +1,33 @@
-// // LECTURE: Values and Variables
+////////////////////////////////////////////////////////////
 
-// 1. Declare variables called 'country','continent' and 'population' and assign their values according to your own country (population in millions)
-// 2. Log their values to the console
+// CHALLENGE #1
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
 
-// let country = "Sweden";
-// let continent = "Europe";
-// let population = "10 420 000";
+// Your task is to write some code to help them:
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+// Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
 
-// LECTURE: Data Types
+// Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
 
-// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// Log the value of BMIMark and BMIJohn to the console.
 
-// let isIsland = false;
-// let language;
+// BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
 
-// 2. Log the types of 'isIsland','population','country' and 'language' to the console.
+// TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
 
-// console.log(isIsland, population, country, language);
+// 👋 OPTIONAL: You can watch my solution in video format in the next lecture
 
-//LECTURE: let, const and var
-//1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
-//2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
-//3. Try to change one of the changed variables now, and observe what happens.
+// IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
 
-const isIsland = false;
-const country = "Sweden";
-const continent = "Europe";
-let population = 10.42;
-const language = "Swedish";
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
 
-// console.log(isIsland, population, country, language);
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
 
-// LECTURE: Basic Operators
-// 1. If your country wascsplit in half,and each half would contain half the population, then how many people would live in each half?
+console.log(BMIMark, BMIJohn);
 
-console.log(typeof population);
-console.log(population / 2);
-
-// 2. Increase the population of your country by 1 and log the result to the console
-
-population++;
-console.log(population);
-
-// 3. Finland has a population of 6 million. Does your country have more people than Finland?
-
-console.log(population > 6);
-
-// 4. The average population of a country is 33 million people. Does your country have less people than the average country?
-
-console.log(population < 33);
-
-// 5. Based on the variables you created create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'
-
-const description =
-  country +
-  " " +
-  "is in" +
-  " " +
-  continent +
-  ", " +
-  "and its " +
-  population +
-  " " +
-  "million people speak" +
-  " " +
-  language;
-console.log(description);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
