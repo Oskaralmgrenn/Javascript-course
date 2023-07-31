af7ecdfad7670ba3c45ebde53c821db637bba5bc
Oskar Almgren <oskaralmgren.oa@gmail.com>
2023-07-31 22:12:06 +0200

    Callenge Let, Const, Var

--- assignments.js
+++ assignments.js
@@ -3,9 +3,9 @@
 // 1. Declare variables called 'country','continent' and 'population' and assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
-let country = "Sweden";
-let continent = "Europe";
-let population = "10 420 000";
+// let country = "Sweden";
+// let continent = "Europe";
+// let population = "10 420 000";
 
 // console.log(country);
 // console.log(continent);
@@ -15,12 +15,22 @@ let population = "10 420 000";
 
 // 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
 
-let isIsland = false;
-let language;
+// let isIsland = false;
+// let language;
 
 // 2. Log the types of 'isIsland','population','country' and 'language' to the console.
+
+// console.log(isIsland, population, country, language);
 
 //LECTURE: let, const and var
 //1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
 //2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
 //3. Try to change one of the changed variables now, and observe what happens.
+
+// const isIsland = false;
+// const country = "Sweden";
+// const continent = "Europe";
+// const population = "10 420 000";
+// language = "Swedish";
+
+// console.log(isIsland, population, country, language);
