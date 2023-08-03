efbb990cf3c24cc23876b2a2cb7826233900e20f
Oskar Almgren <oskaralmgren.oa@gmail.com>
2023-08-03 22:30:06 +0200

    Taking decisions and if/else statements

--- assignments.js
+++ assignments.js
@@ -2,32 +2,56 @@
 
 // Strings and template literals
 
-const firstName = "Oskar";
+// const firstName = "Oskar";
+// const birthYear = 1997;
+// const job = "programmer";
+// const year = 2023;
+
+// const oskar =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+
+// console.log(oskar);
+
+// // Template literals makes it easier for you to write sting scentences
+
+// const oskarNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+
+// console.log(oskarNew);
+
+// console.log(`Just a normal string...`);
+
+// console.log(
+//   "String\n\
+// with\n\
+// multiple\n\
+// lines"
+// );
+
+// console.log(`String
+// with
+// multiple
+// lines`);
+
+// Taking decisions and if/else statements
+
+const age = 15;
+//const isOldEnough = age >= 18; //Not needed. Just write this value in the if statement instead
+
+if (age >= 18) {
+  console.log(`Oskar can start taking his driverslicense 🏎`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Oskar is too young, wait another ${yearsLeft} years`);
+}
+
 const birthYear = 1997;
-const job = "programmer";
-const year = 2023;
 
-const oskar =
-  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+let century;
 
-console.log(oskar);
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
 
-// Template literals makes it easier for you to write sting scentences
-
-const oskarNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
-
-console.log(oskarNew);
-
-console.log(`Just a normal string...`);
-
-console.log(
-  "String\n\
-with\n\
-multiple\n\
-lines"
-);
-
-console.log(`String
-with
-multiple
-lines`);
+console.log(century);
