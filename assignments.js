f0ef9497cb16ebccfbd61c7d99e25295c0b6e88a
Oskar Almgren <oskaralmgren.oa@gmail.com>
2023-08-03 21:48:04 +0200

    Strings and template literals

--- assignments.js
+++ assignments.js
@@ -1,33 +1,33 @@
-////////////////////////////////////////////////////////////
+//
 
-// CHALLENGE #1
-// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+// Strings and template literals
 
-// Your task is to write some code to help them:
+const firstName = "Oskar";
+const birthYear = 1997;
+const job = "programmer";
+const year = 2023;
 
-// Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+const oskar =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
 
-// Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+console.log(oskar);
 
-// Log the value of BMIMark and BMIJohn to the console.
+// Template literals makes it easier for you to write sting scentences
 
-// BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+const oskarNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 
-// TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+console.log(oskarNew);
 
-// 👋 OPTIONAL: You can watch my solution in video format in the next lecture
+console.log(`Just a normal string...`);
 
-// IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+console.log(
+  "String\n\
+with\n\
+multiple\n\
+lines"
+);
 
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-
-console.log(BMIMark, BMIJohn);
-
-const markHigherBMI = BMIMark > BMIJohn;
-console.log(markHigherBMI);
+console.log(`String
+with
+multiple
+lines`);
